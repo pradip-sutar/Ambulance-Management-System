d9bb1eace13e01cdceb0f3ea57aeaa02b3d88138
pradip-sutar <pradip.sutar2018@gmail.com>
2026-05-23 18:27:23 +0530

    Ready for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,30 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+
+  base: "/",
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true
+  },
+
+  server: {
+    port: 4179,
+    http2: false,
+    host: true
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    port: 4179,
+    allowedHosts: ["moambulanceseba.com"],
+  },
+});
