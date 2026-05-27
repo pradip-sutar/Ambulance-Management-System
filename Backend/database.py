fc68ba85bb193829176ca26e27718f0d45a78eb9
6371402510 <jenasoumya385@gmail.com>
2026-05-27 13:38:29 +0530

    build successfull

--- Backend/database.py
+++ Backend/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE_URL = "postgresql://ambulance:Rs$1gsYh2@187.127.169.107:5432/ambudb"
-DATABASE_URL = "postgresql://postgres:9090@localhost:5432/ambulance_db"
+DATABASE_URL = "postgresql://ambulance:Rs$1gsYh2@187.127.169.107:5432/ambudb"
+# DATABASE_URL = "postgresql://postgres:9090@localhost:5432/ambulance_db"
 
 engine = create_engine(DATABASE_URL)
 
